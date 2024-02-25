9c4f5177f5da9960f6390e1091da08b33782eb8f
Jarryd Hoffman <jarryd.hoffman@windowslive.com>
2024-02-25 16:38:07 +1000

    Testing blade directives

--- src/GiraffeUIServiceProvider.php
+++ src/GiraffeUIServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace JayAitch\GiraffeUI;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use JayAitch\GiraffeUI\Components\Button;
 
 class GiraffeUIServiceProvider extends ServiceProvider
 {
@@ -15,16 +17,18 @@ class GiraffeUIServiceProvider extends ServiceProvider
     {
         // Load your package routes, views, assets, etc.
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'giraffeui');
-        
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'giraffeui');
+
+        Blade::component('GiraffeUI::button', Button::class);
+
         // $this->publishes([
         //     __DIR__.'/path/to/assets' => public_path('jayaitch/giraffeui'),
         // ], 'public');
 
         // Register your button component view
-        $this->loadViewComponentsAs('giraffeui', [
-            \JayAitch\GiraffeUI\Components\Button::class => 'giraffeui::button',
-        ]);
+        // $this->loadViewComponentsAs('giraffeui', [
+        //     \JayAitch\GiraffeUI\Components\Button::class => 'giraffeui::button',
+        // ]);
     }
 
     /**
